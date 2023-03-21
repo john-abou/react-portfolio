b1c1b4b040bd2559004468cbf57e21b035f5daa9
john-abou <abounassar.john@gmail.com>
2023-03-21 17:54:14 -0400

    Feat: Added the JSX code to the About component.
    Still need to update the place holder image with my real image.

---
+++ src/components/pages/About.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <section>
+      <h1 id="about">About Me</h1>
+      <img src="https://via.placeholder.com/150" alt="My face" />
+      <p>
+      Welcome to my portfolio website! My name is John Abounassar, and I'm a Fullstack Web Developer with a passion for creating exceptional mobile-first web applications. With a background in data science and a degree in engineering, I bring a unique set of skills and knowledge to every project I work on.
+
+      My expertise includes Javascript, HTML, CSS, Node.js, MySQL, MongoDB, Express.js, and React.js, and I'm constantly expanding my knowledge and exploring new technologies. I've contributed to several successful projects, including LoveLink, a full-stack dating application, and WeTheNorth, a front-end application that uses the balldontlie and SeatGeek API to display NBA game information.
+
+      During my time as an Operations Coordinator at Swyft, I managed over 2,000 daily deliveries, reducing costs by 10% and implemented improved route optimization strategies.
+
+      When I'm not developing web applications, I enjoy weight lifting, pick-up basketball, playing the piano, and watching sitcoms. I believe that having a well-rounded life outside of work helps me bring fresh perspectives and creativity to my projects.
+
+      Please check out my Github and portfolio links to see some of my latest projects and connect with me on LinkedIn to stay in touch. I'm excited about the possibilities that web development holds, and I'm always looking for new opportunities to create something amazing.
+      </p>
+    </section>
+  );
+}
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -2,24 +2,46 @@ import React form 'react';
 
 
 export default function Portfolio() {
+  
+  const projects = [
+    {
+      name: 'Project 1',
+      description: 'This is a project',
+      github: '',
+      deployed: '',
+      image: ''
+    }, {
+      name: 'Project 2',
+      description: 'This is a project',
+      github: '',
+      deployed: '',
+      image: ''
+    }, {
+      name: 'Project 3',
+      description: 'This is a project',
+      github: '',
+      deployed: '',
+      image: ''
+    }, {
+      name: 'Project 4',
+      description: 'This is a project',
+      github: '',
+      deployed: '',
+      image: ''
+    }, {
+      name: 'Project 5',
+      description: 'This is a project',
+      github: '',
+      deployed: '',
+      image: ''
+    }, {
+      name: 'Project 6',
+      description: 'This is a project',
+      github: '',
+      deployed: '',
+      image: ''
+    }
+  ];
   return (
-    <section className="row">
-      {/* Make a fetch to Github's RESTful API to get my 6 featured projects  */}
-      axios.get('https://api.github.com/users/johnabounassar/repos')
-
-      {/* Loop through and create grid like thing for the 6 featured projects on my github account */}
-      {
-        <div className="col-lg-4 col-md-6">
-          <div className="card">
-            <img src="..." className="card-img-top" alt="..."/>
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" className="btn btn-primary">Go somewhere</a>
-            </div>
-          </div>
-        </div>
-      }
-    </section>
   );
 }
