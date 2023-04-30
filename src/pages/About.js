c1c9070099267f78f07db61f0c57046e2e331877
john-abou <abounassar.john@gmail.com>
2023-04-29 21:53:47 -0400

    Feat: Made the about page fully responsive.

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,37 +1,32 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import photo from '../assets/images/john.png'
+import LaptopIcon from '@material-ui/icons/Laptop';
+import SmartphoneIcon from '@material-ui/icons/Smartphone';
+import CheckBoxIcon from '@material-ui/icons/CheckBox';
 
 export default function About() {
+  let width = window.innerWidth;
+  const isTablet = width < 778;
 
   return (
     <div className='container-fluid'>
       <section id="about" className='row'>
         <div className='col-md-6 col-sm-12'>
-          <article className="about-text">
-            {/* <p >
-              I am a highly motivated and detail-oriented Fullstack Web Developer with a passion for creating engaging and user-friendly websites. My expertise lies in Javascript, HTML, CSS, Node.js, MySQL, MongoDB, Express.js, and React.js. As a lifelong learner, I am always exploring new technologies to stay up-to-date with the latest trends and advancements in web development.
-            </p>
-            <p>
-              Before transitioning into web development, I worked as an operations coordinator at a startup in San Francisco. There, I successfully managed the lifecycle of over 2,000 daily deliveries, reduced operating costs by 10%, and analyzed client data to make data-driven decisions for market expansion.
-            </p>
-            <p>
-              My experience in operations has honed my ability to problem-solve, think critically, and work collaboratively with others. I apply these skills to my work as a web developer to create functional and visually appealing websites that meet the unique needs of each client.
-            </p>
-            <p>
-              I am committed to delivering high-quality work and exceeding client expectations. Let's work together to create a website that showcases your brand and drives results.and connect with me on LinkedIn to stay in touch. I'm excited about the possibilities that web development holds, and I'm always looking for new opportunities to create something amazing.
-            </p> */}
-            <h1 className='about-header'>Full Stack Web Developer. <span>John Abounassar</span></h1>
+          <article className="about-text d-flex flex-column justify-content-center">
+            <h1 className='about-header'>Full Stack Web Developer.</h1>
+            <h2 className='about-name'>John Abounassar</h2>
             <div className='about-description'>
-              <h3 className='mx-2 fs-1'>Specializing in: </h3>
-              <p className='skill-one fs-3'>Front-end Development</p>
-              <p className='skill-two fs-3'>Responsive Design</p>
-              <p className='skill-three fs-3'>Problem Solving</p>
-            
+                <div className='d-flex flex-column justify-content-center'>
+                  <h3 className='mx-2'>Specializing in: </h3>
+                  <p className='skill-one'><LaptopIcon fontSize='large'/> Front-end Development</p>
+                  <p className='skill-two'><SmartphoneIcon fontSize='large'/> Responsive Design</p>
+                  <p className='skill-three'><CheckBoxIcon fontSize='large'/> Problem Solving</p>
+                </div>
             </div>
           </article>
         </div>
         <div className='col-md-6 col-sm-12'>
-          <aside>
+          <aside className='d-flex flex-column justify-content-center'>
             {<img src={photo} className="about-photo" alt="My face" />}
           </aside>
         </div>
