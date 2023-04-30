b41d11b5d76a365b8f47887c5249de68f8b7995b
john-abou <abounassar.john@gmail.com>
2023-04-29 22:11:27 -0400

    Fix: Fixed the responsive CSS for the about section.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ import {faGithub, faLinkedin, faMedium} from '@fortawesome/free-brands-svg-icons
 export default function Footer() {
 
   return (
-  <footer className="fixed-bottom">
+  <footer>
     <div className='links d-flex flex-row justify-content-center align-items-center mt-3 mb-3'>
       <a href="https://github.com/john-abou" className='nav-link'> <FontAwesomeIcon icon={faGithub} size="4x" /></a>
       <a href="https://www.linkedin.com/in/john-abounassar/" className='nav-link'><FontAwesomeIcon icon={faLinkedin} size="4x" /></a>
